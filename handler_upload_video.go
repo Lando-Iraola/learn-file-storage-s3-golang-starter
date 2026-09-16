3da5a9e7b7aaf1758e97b6a50e011d0cc6d2946c
Lando <lando_vargas@outlook.com>
2026-09-16 01:10:02 -0300

    CH3: L6

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,129 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	fmt.Println("uploading video", videoID, "by user", userID)
+
+	// TODO: implement the upload here
+
+	const maxMemory = 1 << 30
+	err = r.ParseMultipartForm(maxMemory)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse form data", err)
+		return
+	}
+
+	mFile, mFileHeader, err := r.FormFile("video")
+	defer mFile.Close()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't find thumbnail", err)
+		return
+	}
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read video", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't find video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User is not authorized to modify this resource", err)
+		return
+	}
+
+	mediaType := mFileHeader.Header.Get("Content-Type")
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Missing Content-Type for humbnail", nil)
+		return
+	}
+
+	mt, _, err := mime.ParseMediaType(mediaType)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to parse Content-Type", err)
+		return
+	}
+
+	if mt != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Media-Type not accepted", nil)
+		return
+	}
+
+	rndKey := make([]byte, 32)
+	rand.Read(rndKey)
+	rndKeyTxt := base64.RawURLEncoding.EncodeToString(rndKey)
+
+	fileName := fmt.Sprintf("%s%s", rndKeyTxt, ".mp4")
+	filePointer, err := os.CreateTemp(cfg.assetsRoot, "tabuly-video")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to create file", err)
+		return
+	}
+
+	_, err = io.Copy(filePointer, mFile)
+	defer os.Remove(filePointer.Name())
+	defer filePointer.Close()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to copy file contents", err)
+		return
+	}
+	filePointer.Seek(0, io.SeekStart)
+	cfg.s3Client.PutObject(context.Background(), &s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &fileName,
+		Body:        filePointer,
+		ContentType: &mt,
+	},
+	)
+
+	url := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, fileName)
+	video.VideoURL = &url
+	err = cfg.db.UpdateVideo(video)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
+}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 
 	"github.com/joho/godotenv"
@@ -17,6 +20,7 @@ type apiConfig struct {
 	platform         string
 	filepathRoot     string
 	assetsRoot       string
+	s3Client         *s3.Client
 	s3Bucket         string
 	s3Region         string
 	s3CfDistribution string
@@ -92,6 +96,17 @@ func main() {
 		s3CfDistribution: s3CfDistribution,
 		port:             port,
 	}
+
+	awsCfg, err := config.LoadDefaultConfig(
+		context.Background(),
+		config.WithRegion(cfg.s3Region))
+	if err != nil {
+		log.Fatalf("Couldn't load AWS config")
+	}
+
+	s3client := s3.NewFromConfig(awsCfg)
+
+	cfg.s3Client = s3client
 
 	err = cfg.ensureAssetsDir()
 	if err != nil {
